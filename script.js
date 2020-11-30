a8604c2db0831eda9bdf387faffa7b44a2799ea4
Ruginn Catarata <Ruginn@Yip-Yip.attlocal.net>
2020-11-30 11:17:15 -0500

    js done

---
+++ script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.mesage-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+    //Using Contraint API
+    isValid = form.checkValidity();
+    console.log(isValid);
+    //Stlye main message for an error
+    if (!isValid) {
+    message.textContent = 'Please fill out all fields.'
+    message.style.color = 'red';
+    }
+    // Check to see if password match
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+        return;
+    } else {
+        passwordsMatch = false;
+        message.textContent = 'Make sure password match.';
+        message.style.color = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+    // If form is valid and passwords match
+    if (isValid && passwordsMatch) {
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.website.value
+    };
+    // Move user data
+    console.log(user);
+}
+function processFormData(e) {
+    e.preventDefault();
+    //Validate Form 
+    validateForm();
+    // Submit Data if Valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
+}
+//Event Listener 
+form.addEventListener('submit', processFormData);
